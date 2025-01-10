cb82e69f91be11be9971b87c7996a5254eac640c
Tanmay Goyal <gotanmay@gmail.com>
2025-01-10 10:07:00 +0530

    updated gitignore

--- configs.sh
+++ configs.sh
@@ -3,23 +3,25 @@
 
 
 python3 main.py \
---dataset glue/mrpc \
+--dataset glue/sst2 \
 --seed 2 \
 --num_shots 4 \
 --example_pool_size 16 \
 --embedding_dim 64 \
 --failure_level 0.05 \
 --repeat_examples \
---load_chkpt_idx 1794 \
---load_data_path glue_mrpc_result/09-01_06-54
+--warmup_length 4128 \
+--only_test
 
 python3 main.py \
---dataset glue/mrpc \
+--dataset glue/sst2 \
 --seed 2 \
 --num_shots 4 \
 --example_pool_size 32 \
 --embedding_dim 64 \
 --failure_level 0.05 \
---repeat_examples
+--repeat_examples \
+--warmup_length 4128 \
+--only_test
 
 
